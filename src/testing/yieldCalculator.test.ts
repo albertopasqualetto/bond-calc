863138e4a3c5c55cc8ec1ff3e9eed8d10d8a6fda
Alberto Pasqualetto <39854348+albertopasqualetto@users.noreply.github.com>
2025-04-01 19:33:21 +0200

    More tests

--- src/testing/yieldCalculator.test.ts
+++ src/testing/yieldCalculator.test.ts
@@ -9,7 +9,7 @@ describe('Bond Yield Calculator', () => {
 	test.todo('Short duration bond (2 years)');
 	test.todo('Annual payment frequency');
 
-	test('IT0005445306 @ 07/03/2025', () => {
+	test('IT0005445306 @ 07/03/2025 1 digit prec', () => {
 		const bond = new FinancialAsset(
 			"IT0005445306",
 			createDate(2025, 3, 7),
@@ -21,7 +21,7 @@ describe('Bond Yield Calculator', () => {
 		);
 		const annualYield = bond.computeYield();
 		console.log(annualYield);
-		expect(annualYield).toBeCloseTo(2.781, 0);
+		expect(annualYield).toBeCloseTo(2.781, 1);
 	});
 
 	test('IT0005441883 @ 05/03/2025', () => {
@@ -36,7 +36,7 @@ describe('Bond Yield Calculator', () => {
 		);
 		const annualYield = bond.computeYield();
 		console.log(annualYield);
-		expect(annualYield).toBeCloseTo(4.2675, 0);
+		expect(annualYield).toBeCloseTo(4.2675, 2);
 	});
 	test('after 1 year today same price cut', () => {
 		const today = new Date();
@@ -48,6 +48,7 @@ describe('Bond Yield Calculator', () => {
 		maturityDate.setFullYear(maturityDate.getFullYear() + 2);
 		const couponRatePerc = 0.50;
 		const settlementPrice = 90;
+		const oneYearYield = couponRatePerc/settlementPrice*100;
 
 		const bond = new FinancialAsset(
 			undefined,
@@ -61,10 +62,10 @@ describe('Bond Yield Calculator', () => {
 		console.log("Cashflows today", bond.toIrrCashflows(today, settlementPrice));
 		const annualYield = bond.computeYield(today, settlementPrice);
 		console.log(annualYield);
-		expect(annualYield).toBeCloseTo(couponRatePerc, 0);
+		expect(annualYield).toBeCloseTo(oneYearYield, 2);
 	});
 
-	test('IT0005024234 @ 31/03/2025', () => {
+	test('IT0005024234 @ 31/03/2025 1 digit prec', () => {
 		const bond = new FinancialAsset(
 			"IT0005024234",
 			createDate(2025, 3, 31),
@@ -77,6 +78,27 @@ describe('Bond Yield Calculator', () => {
 		console.log("Cashflows", bond.toIrrCashflows());
 		const annualYield = bond.computeYield();
 		console.log(annualYield);
-		expect(annualYield).toBeCloseTo(2.86, 0);
+		expect(annualYield).toBeCloseTo(2.86, 1);
+	});
+
+	test('IT0005494239 sell 1 day after net', () => {
+		const bond = new FinancialAsset(
+			"IT0005494239",
+			createDate(2025, 3, 31),
+			createDate(2032, 12, 1),
+			2.5,
+			93.87,
+			100,
+			2,
+			12.5
+		);
+		const today = createDate(2025, 4, 1);
+		const todayPrice = 94;
+		console.log("Cashflows today",
+			bond.toIrrCashflowsNet(today,
+													todayPrice));
+		const annualYield = bond.computeYieldNet(today, 						todayPrice);
+		console.log(annualYield);
+		expect(annualYield).toBeCloseTo(59.13, 1);
 	});
 });
